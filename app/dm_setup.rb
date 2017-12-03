8a76413df7490e20a430aed7e9ae96b3bfb2783a
Tabish Raza <tabishraza@gmail.com>
2017-12-03 15:36:43 +0000

    require dbs

--- app/dm_setup.rb
+++ app/dm_setup.rb
@@ -2,7 +2,12 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 require_relative 'models/comment'
+require_relative 'models/interest'
+require_relative 'models/follower'
+require_relative 'models/like'
+require_relative 'models/marked_solution'
 require_relative 'models/post'
+require_relative 'models/tag'
 require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/stack_#{ENV['RACK_ENV']}")
