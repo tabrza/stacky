8c3b0ae1dd448135f6099b2a61e7d6bb95e2d45c
Tabish Raza <tabishraza@gmail.com>
2017-12-04 10:02:57 +0000

    naming error correction

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,12 @@
-class Post
+class Comment
   include DataMapper::Resource
 
   property :id, Serial
   property :maker, String
   property :body, Text
   property :created_at, DateTime
+
+  has n, :likes
 
   belongs_to :user
   belongs_to :post
