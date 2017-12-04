140bc9a79c9b761fb82f2e371db01875f5cbfe07
Tabish Raza <tabishraza@gmail.com>
2017-12-04 21:37:52 +0000

    sign in tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,9 +6,26 @@ def sign_in(email, password)
 end
 
 feature 'sign in' do
+
   scenario 'user can sign in' do
     signup('test name', 'test', '123@test.com', '12345', '12345')
+    click_button 'Sign out'
     sign_in('123@test.com', '12345')
     expect(page).to have_content('posts')
   end
+
+  scenario 'user cannot sign in with wrong email' do
+    signup('test name', 'test', '123@test.com', '12345', '12345')
+    click_button 'Sign out'
+    sign_in('223@test.com', '12345')
+    expect(page).to have_content('Incorrect email or password')
+  end
+
+  scenario 'user cannot sign in with wrong password' do
+    signup('test name', 'test', '123@test.com', '12345', '12345')
+    click_button 'Sign out'
+    sign_in('123@test.com', '22345')
+    expect(page).to have_content('Incorrect email or password')
+  end
+
 end
